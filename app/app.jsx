1183c9026526a6250d23783b8e4b6c674d5aa0a6
reniur <62297326+taindw000w@users.noreply.github.com>
2022-09-15 13:38:47 +0700

    Add files via upload

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 import "./app.css";
 
@@ -7,204 +7,142 @@ import { TaskList } from "../taskList/taskList";
 import { Footer } from "../footer/footer";
 import { NewTaskForm } from "../newTaskForm/newTaskForm";
 
-export class App extends React.Component {
-  constructor(props) {
-    super(props);
-    const { initialTasks, initialFilter } = this.props;
-    
-    this.currentID = 1;
-
-    this.incrementID = () => {
-      this.currentID += this.currentID + 1;
-      return this.currentID;
-    };
-
-    this.createTask = (
+export const App = ({ initialTasks, initialFilter }) => {
+  const createTask = (description, seconds) => {
+    window.id += 1;
+    return {
       description,
-      time = 0,
-      isDone = false,
-      isEditing = false,
-      createdDate = new Date(),
-      id = this.incrementID()
-    ) => {
-      return {
-        description,
-        isDone,
-        isEditing,
-        time,
-        createdDate,
-        id,
-      };
+      seconds,
+      id: window.id,
     };
+  };
 
-    this.state = {
-      tasksData: initialTasks.map((task) => {
-        return this.createTask(task.description, task.time, task.isDone, task.isEditing);
-      }),
-      filter: initialFilter,
-    };
+  const [tasksData, setTasksData] = useState([]);
 
-  }
+  const [filter, setFilter] = useState(initialFilter);
 
-  render() {
-    const { tasksData, filter} = this.state;
+  useEffect(() => {
+    window.id = 0;
+    setTasksData(
+      initialTasks.map((task) => {
+        const { description, seconds } = task;
 
-    const convertToNumber = (string) => {
-      const integer = parseInt(string, 10);
-      return Number.isNaN(integer) ? 0 : integer;
-    };
-
-    const addTask = (label, min = 0, sec = 0) => {
-      if (label === "" )   return null;
-      const newTask = this.createTask(
-        label,
-        convertToNumber(min) * 60 + convertToNumber(sec)
-      );
-
-      this.setState((state) => {
-        const newTasksData = [...state.tasksData, newTask];
-
-        return {
-          tasksData: newTasksData,
-        };
-      });
-    };
-
-    const findIndexByID = (id) => {
-      const { tasksData } = this.state;
-
-      return tasksData.findIndex((task) => task.id === id);
-    };
-
-    const toggleProperty = (property, id) => {
-      this.setState((state) => {
-        const index = findIndexByID(id);
-
-        const modifiedTaskData = {
-          ...state.tasksData[index],
-          [property]: !state.tasksData[index][property],
-        };
-
-        const modifiedTasksData = [
-          ...state.tasksData.slice(0, index),
-          modifiedTaskData,
-          ...state.tasksData.slice(index + 1),
-        ];
-
-        return {
-          tasksData: modifiedTasksData,
-        };
-      });
-    };
-
-    const deleteTask = (id) => {
-      this.setState((state) => {
-        const index = findIndexByID(id);
-
-        const newTasksData = [
-          ...state.tasksData.slice(0, index),
-          ...state.tasksData.slice(index + 1),
-        ];
-        
-        return {
-          tasksData: newTasksData,
-        };
-      });
-    };
-
-    const handleFilterChange = (newFilter) => {
-      this.setState({
-        filter: newFilter,
-      });
-    };
-
-    const clearCompleted = () => {
-      const { tasksData } = this.state;
-
-      const activeTasks = tasksData.filter((task) => !task.isDone);
-
-      this.setState({
-        tasksData: activeTasks,
-      });
-    };
-
-    const countActiveTasks = () => {
-      const { tasksData } = this.state;
-
-      return tasksData.filter((task) => !task.isDone).length;
-    };
-
-    const changeDescription = (description, id) => {
-      this.setState((state) => {
-        const index = findIndexByID(id);
-
-        const modifiedTaskData = {
-          ...state.tasksData[index],
-          description,
-        };
-
-        const modifiedTasksData = [
-          ...state.tasksData.slice(0, index),
-          modifiedTaskData,
-          ...state.tasksData.slice(index + 1),
-        ];
-
-        return {
-          tasksData: modifiedTasksData,
-        };
-      });
-    };
-
-    const finishEditing = (id) => {
-      this.setState((state) => {
-        const index = findIndexByID(id);
-
-        const modifiedTaskData = {
-          ...state.tasksData[index],
-          isEditing: false,
-        };
-
-        const modifiedTasksData = [
-          ...state.tasksData.slice(0, index),
-          modifiedTaskData,
-          ...state.tasksData.slice(index + 1),
-        ];
-
-        return {
-          tasksData: modifiedTasksData,
-        };
-      });
-    };
-
-    return (
-      <div>
-        <section className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <NewTaskForm 
-              onAdd={addTask} 
-            />
-          </header>
-          <section className="main">
-            <TaskList
-              tasksData={tasksData}
-              onToggleProperty={toggleProperty}
-              filter={filter}
-              onChangeDescription={changeDescription}
-              onFinishEditing={finishEditing}
-              onDelete={deleteTask}
-            />
-            <Footer
-              filter={filter}
-              onFilterChange={handleFilterChange}
-              onClearCompleted={clearCompleted}
-              activeTasksCount={countActiveTasks}
-            />
-          </section>
-        </section>
-      </div>
+        return createTask(description, seconds);
+      })
     );
+  }, [initialTasks]);
+
+  const convertToNumber = (string) => {
+    const integer = parseInt(string, 10);
+    return Number.isNaN(integer) ? 0 : integer;
+  };
+
+  const addTask = (label, min = 0, sec = 0) => {
+    if (label === '' || isNaN(min) || isNaN(sec)) return null;
+
+    const minInt = convertToNumber(min);
+    const secInt = convertToNumber(sec);
+    const newTask = createTask(label, minInt * 60 + secInt);
+
+    setTasksData((tasks) => {
+      return [...tasks, newTask];
+    });
+  };
+
+  const findIndexByID = (id, tasks) => {
+    return tasks.findIndex((task) => task.id === id);
+  };
+
+  const getChangedProperty = (property, task, changeVariant = 'toggle', value = null) => {
+    switch (changeVariant) {
+      case 'toggle':
+        return !task[property];
+      case 'decrement':
+        if (task[property] <= 0) return task[property]
+        return task[property] - 1;
+      case 'value':
+        return value;
+      default:
+        return value;
+    }
+  };
+
+  const changeProperty = (property, id, changeVariant, propertyShouldNotBe = null, value = null) => {
+    setTasksData((tasks) => {
+      const index = findIndexByID(id, tasks);
+  
+      if (tasks[index][property] === propertyShouldNotBe) {
+        return tasks;
+      }
+
+      const modifiedTaskData = {
+        ...tasks[index],
+        [property]: getChangedProperty(property, tasks[index], changeVariant, value),
+      };
+
+      const modifiedTasksData = [...tasks.slice(0, index), modifiedTaskData, ...tasks.slice(index + 1)];
+
+      return modifiedTasksData;
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasksData((tasks) => {
+      const index = findIndexByID(id, tasks);
+
+      return [...tasks.slice(0, index), ...tasks.slice(index + 1)];
+    });
+  };
+
+  const clearCompleted = () => {
+    setTasksData((tasks) => {
+      return tasks.filter((task) => !task.isDone);
+    });
+  };
+
+  const countActiveTasks = () => {
+    return tasksData.filter((task) => !task.isDone).length;
+  };
+
+  const changeDescription = (description, id) => {
+    changeProperty('description', id, 'value', null, description);
+  };
+
+  const finishEditing = (id) => {
+    changeProperty('isEditing', id, 'toggle', false);
+  };
+
+  const tick = (id) => {
+    changeProperty('seconds', id, 'decrement');
   }
+
+   return (
+    <div>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <NewTaskForm onAdd={addTask} />
+        </header>
+        <section className="main">
+          <TaskList
+            tasksData={tasksData}
+            onToggleProperty={changeProperty}
+            filter={filter}
+            onChangeDescription={changeDescription}
+            onFinishEditing={finishEditing}
+            onDelete={deleteTask}
+            onTick={tick}
+          />
+          <Footer
+            filter={filter}
+            onFilterChange={setFilter}
+            onClearCompleted={clearCompleted}
+            activeTasksCount={countActiveTasks}
+          />
+        </section>
+      </section>
+    </div>
+  );
 }
 
 App.defaultProps = {
@@ -216,4 +154,3 @@ App.propTypes = {
   initialTasks: PropTypes.arrayOf(PropTypes.object),
   initialFilter: PropTypes.oneOf(["All", "Active", "Completed"]),
 };
-
