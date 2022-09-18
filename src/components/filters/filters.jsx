27c2076cf3205413b03963cdba31e13824a50468
reniur <62297326+taindw000w@users.noreply.github.com>
2022-09-18 15:26:19 +0700

    Add files via upload

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -3,46 +3,28 @@ import PropTypes from "prop-types";
 
 import "./filters.css";
 
-const classSelected = 'selected';
+const TasksFilter = ({ filter, onFilterChange }) => {
+  const buttons = [ 
+    { name: 'All', label: 'All'},
+    { name: 'Active', label: 'Active'},
+    { name: 'Completed', label: 'Completed'},
+  ]
+  const destructureFilters = buttons.map(({name, label}) => {
+    const isActive = filter === name;
+    const className = isActive ? 'selected' : '';
 
-const TasksFilter = ({ onFilterChange }) => {
-  const getFilterButtons = () => {
-    return document.querySelectorAll('.filters button');
-  };
-
-  const onClick = (event) => {
-    if (event.target.classList.contains(classSelected)) {
-      return;
-    }
-
-    const buttons = getFilterButtons();
-    for (const button of buttons) {
-      button.classList.remove(classSelected);
-    }
-
-    event.target.classList.add(classSelected);
-    onFilterChange(event.target.textContent);
-  };
-
+    return (
+      <li key={name}>
+        <button className={`${className}`} type="button" onClick={()=> onFilterChange(name)}>
+          {label}
+        </button>
+      </li>
+    )
+  })
+  
   return (
-    <ul className="filters">
-      <li>
-        <button className="selected" type="button" onClick={onClick}>
-          All
-        </button>
-      </li>
-      <li>
-        <button type="button" onClick={onClick}>
-          Active
-        </button>
-      </li>
-      <li>
-        <button type="button" onClick={onClick}>
-          Completed
-        </button>
-      </li>
-    </ul>
-  );
+    <ul className="filters">{destructureFilters}</ul>
+  )
 };
 
 TasksFilter.propTypes = {
